3b6f2f3b7611c50775e431e02aa56072a28f2b9c
binthon <jakub@medynwp.pl>
2023-12-09 20:21:58 +0100

    ADD NFJ and Just Join

--- main.py
+++ main.py
@@ -4,89 +4,76 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
-import time 
+import re
 
-option = Options()
-option.headless = True
-
+options = Options() 
+options.add_argument("-headless") 
+driver = webdriver.Firefox(options=options) 
 def choiceCloud():
     cloudName = input("Enter the name of the cloud service provider whose number of offers you want to know: ")
-    if (cloudName.lower() == "aws") or (cloudName.lower() == "amazon web services") or (cloudName.lower() == "amazon"):
-        countAws(cloudName.lower())
-    elif (cloudName.lower() == "azure") or (cloudName.lower() == "microsoft azure"):
-        countAzure(cloudName.lower())
-    elif (cloudName.lower() == "google cloud") or (cloudName.lower() == "google cloud platform") or (cloudName.lower() == "gcp"):
-        countGcp(cloudName.lower())
-    elif (cloudName.lower() == "ibm cloud"):
-        countIBM(cloudName.lower())
-    elif (cloudName.lower() == "oracle cloud"):
-        countOracle(cloudName.lower())
+    if (cloudName.lower() == "aws") or (cloudName.lower() == "amazon web services") or \
+    (cloudName.lower() == "amazon") or (cloudName.lower() == "google cloud") or \
+    (cloudName.lower() == "azure") or (cloudName.lower() == "microsoft azure") or \
+    (cloudName.lower() == "ms azure") or (cloudName.lower() == "google cloud platform") or \
+    (cloudName.lower() == "gcp") or (cloudName.lower() == "ibm cloud") or (cloudName.lower() == "oracle cloud"):
+        countCloud(cloudName.lower())
     
-def countAws(cloudName):
-    driver = webdriver.Firefox(options=option)
-    driver.get(f"https://it.pracuj.pl/praca/{cloudName};kw")
+def countCloud(cloudName):
+
     try:
-        span_element = WebDriverWait(driver, 20).until(
+        driver.get(f"https://it.pracuj.pl/praca/{cloudName};kw")
+        elementPracuj = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.CLASS_NAME, "core_c11srdo1"))
         )
-        span_text = span_element.text
-        print(f"The number of offers for {cloudName} is: {span_text}")
+        quantityAWS = elementPracuj.text
+        print(f"The number of offers for {cloudName} in Pracuj.pl is: {quantityAWS}")
+        
+        cloudName = cloudName.replace(" ", "+")
+        driver.get(f"https://justjoin.it/?keyword={cloudName}")
+        elementJoin = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "MuiTab-iconWrapper"))
+        )
+        quantityJoin = re.search(r'(\d{1,3}(?:\s\d{3})*)', elementJoin.text)
+        if quantityJoin:
+            numberJoin = quantityJoin.group(1)
+            numberJoin = numberJoin.replace(" ", "")
+            cloudName = cloudName.replace("+", " ")
+            print(f"The number of offers for {cloudName} in Just Join is: {numberJoin}")
+        else:
+            print("No valid number found in the text.")
+            
+        quantityNFJ = countNFJCloud(cloudName, driver)
+        
+        driver.get(f"https://theprotocol.it/filtry/{cloudName};t")
+        quantityAll = int(quantityAWS) + int(numberJoin) + quantityNFJ
+        print(f"Sum offer is: {quantityAll}")
         driver.quit()
     finally:
         driver.quit()
 
-def countAzure(cloudName):
-    driver = webdriver.Firefox(options=option)
-    driver.get(f"https://it.pracuj.pl/praca/{cloudName};kw")
+def countNFJCloud(cloudName, driver):
+        if( cloudName == "ibm cloud") or (cloudName == "oracle cloud") or (cloudName == "oracle cloud") or (cloudName.lower() == "microsoft azure"):
+            zmienna1, zmienna2 = cloudName.split()
+            driver.get(f"https://nofluffjobs.com/pl/{zmienna2}?criteria=requirement={zmienna1}")
+            WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "list-container"))
+            )
+            link_elements = driver.find_elements(By.CLASS_NAME, "list-container a")
+            quantityNFJ = len(link_elements)
+            print(f"The number of offers for {cloudName} in No Fluff Jobs is: {quantityNFJ}")
 
-    try:
-        span_element = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "core_c11srdo1"))
-        )
-        span_text = span_element.text
-        print(f"The number of offers for {cloudName} is: {span_text}")
-        driver.quit()
-    finally:
-        driver.quit()
+            return quantityNFJ
+        else:
+            driver.get(f"https://nofluffjobs.com/pl/{cloudName}")
+            WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "list-container"))
+            )
+            link_elements = driver.find_elements(By.CLASS_NAME, "list-container a")
+            quantityNFJ = len(link_elements)
+            print(f"The number of offers for {cloudName} in No Fluff Jobs is: {quantityNFJ}")
 
-def countGcp(cloudName):
-    driver = webdriver.Firefox(options=option)
-    driver.get(f"https://it.pracuj.pl/praca/{cloudName};kw")
-    try:
-        span_element = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "core_c11srdo1"))
-        )
-        span_text = span_element.text
-        print(f"The number of offers for {cloudName} is: {span_text}")
-        driver.quit()
-    finally:
-        driver.quit()
-
-def countIBM(cloudName):
-    driver = webdriver.Firefox(options=option)
-    driver.get(f"https://it.pracuj.pl/praca/{cloudName};kw")
-    try:
-        span_element = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "core_c11srdo1"))
-        )
-        span_text = span_element.text
-        print(f"The number of offers for {cloudName} is: {span_text}")
-        driver.quit()
-    finally:
-        driver.quit()
-
-def countOracle(cloudName):
-    driver = webdriver.Firefox(options=option)
-    driver.get(f"https://it.pracuj.pl/praca/{cloudName};kw")
-    try:
-        span_element = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "core_c11srdo1"))
-        )
-        span_text = span_element.text
-        print(f"The number of offers for {cloudName} is: {span_text}")
-        driver.quit()
-    finally:
-        driver.quit()
+            return quantityNFJ
+    
                 
 choiceCloud()
 
