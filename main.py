0f7d26450ca19048ff0d194c297b076e3f7e3c4c
binthon <jakubmedyn@wp.pl>
2023-12-12 23:05:28 +0100

    Add pracuj and join function

--- main.py
+++ main.py
@@ -17,63 +17,106 @@ def choiceCloud():
     (cloudName.lower() == "ms azure") or (cloudName.lower() == "google cloud platform") or \
     (cloudName.lower() == "gcp") or (cloudName.lower() == "ibm cloud") or (cloudName.lower() == "oracle cloud"):
         countCloud(cloudName.lower())
+    else:
+        print("Check cloud name")
     
 def countCloud(cloudName):
 
     try:
-        driver.get(f"https://it.pracuj.pl/praca/{cloudName};kw")
-        elementPracuj = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "core_c11srdo1"))
-        )
-        quantityAWS = elementPracuj.text
-        print(f"The number of offers for {cloudName} in Pracuj.pl is: {quantityAWS}")
-        
-        cloudName = cloudName.replace(" ", "+")
-        driver.get(f"https://justjoin.it/?keyword={cloudName}")
-        elementJoin = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "MuiTab-iconWrapper"))
-        )
-        quantityJoin = re.search(r'(\d{1,3}(?:\s\d{3})*)', elementJoin.text)
-        if quantityJoin:
-            numberJoin = quantityJoin.group(1)
-            numberJoin = numberJoin.replace(" ", "")
-            cloudName = cloudName.replace("+", " ")
-            print(f"The number of offers for {cloudName} in Just Join is: {numberJoin}")
-        else:
-            print("No valid number found in the text.")
-            
+        #scrapping from pracuj
+        quantityPracuj = countPracuj(cloudName, driver)
+
+        #scrapping from just join
+        numberJoin = countJust(cloudName, driver)
+        #scrapping from NFJ 
         quantityNFJ = countNFJCloud(cloudName, driver)
-        
-        driver.get(f"https://theprotocol.it/filtry/{cloudName};t")
-        quantityAll = int(quantityAWS) + int(numberJoin) + quantityNFJ
+
+        #scrapping from protocol
+        numberProtocol = countProtocol(cloudName, driver)
+        #offerSum
+        quantityAll = int(quantityPracuj) + int(numberJoin) + int(quantityNFJ) + int(numberProtocol)
         print(f"Sum offer is: {quantityAll}")
         driver.quit()
     finally:
         driver.quit()
 
+def countPracuj(cloudName, driver):
+    words = cloudName.split()
+    if len(words) > 1:   
+        search_query = '%20'.join(words)
+        driver.get(f"https://it.pracuj.pl/praca/{search_query};kw")
+    else:
+        driver.get(f"https://it.pracuj.pl/praca/{cloudName};kw")
+
+        # Oczekiwanie na element zawierający liczbę ofert pracy
+    elementPracuj = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "core_c11srdo1"))
+    )
+    quantityPracuj = elementPracuj.text
+    print(f"The number of offers for {cloudName} in Pracuj.pl is: {quantityPracuj}")
+
+    return quantityPracuj
+
+def countJust(cloudName, driver):
+    words = cloudName.split()
+    if len(words) > 1:   
+        search_query = '+'.join(words)
+        driver.get(f"https://justjoin.it/?keyword={search_query}")
+    else:
+        driver.get(f"https://justjoin.it/?keyword={cloudName}")
+    elementJoin = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "MuiTab-iconWrapper"))
+    )
+    quantityJoin = re.search(r'(\d{1,3}(?:\s\d{3})*)', elementJoin.text)
+    if quantityJoin:
+        numberJoin = quantityJoin.group(1)
+        numberJoin = numberJoin.replace(" ", "")
+        cloudName = cloudName.replace("+", " ")
+        print(f"The number of offers for {cloudName} in Just Join is: {numberJoin}")
+    else:
+        print("No valid number found in the text.")
+
+    return numberJoin
+
 def countNFJCloud(cloudName, driver):
-        if( cloudName == "ibm cloud") or (cloudName == "oracle cloud") or (cloudName == "oracle cloud") or (cloudName.lower() == "microsoft azure"):
-            zmienna1, zmienna2 = cloudName.split()
-            driver.get(f"https://nofluffjobs.com/pl/{zmienna2}?criteria=requirement={zmienna1}")
-            WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "list-container"))
-            )
-            link_elements = driver.find_elements(By.CLASS_NAME, "list-container a")
-            quantityNFJ = len(link_elements)
-            print(f"The number of offers for {cloudName} in No Fluff Jobs is: {quantityNFJ}")
+    if( cloudName == "ibm cloud") or (cloudName == "oracle cloud") \
+    or (cloudName.lower() == "microsoft azure") or (cloudName.lower() == "ms azure") \
+    or (cloudName.lower() == "amazon web services") or (cloudName.lower() == "google cloud platform") or (cloudName.lower() == "google cloud"):
+        quantityNFJ = 0
+        print("Not key word for this webiste")
 
-            return quantityNFJ
+        return quantityNFJ 
+    else:
+        driver.get(f"https://nofluffjobs.com/pl/{cloudName}")
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "list-container"))
+        )
+        link_elements = driver.find_elements(By.CLASS_NAME, "list-container a")
+        quantityNFJ = len(link_elements)
+        print(f"The number of offers for {cloudName} in No Fluff Jobs is: {quantityNFJ}")
+
+        return quantityNFJ 
+
+def countProtocol(cloudName, driver):
+    if( cloudName == "ibm cloud") or (cloudName == "oracle cloud") or (cloudName.lower() == "ms azure"):
+        numberProtocol = 0
+        print("Not key word for this webiste")
+        return numberProtocol == 0
+    else:
+        driver.get(f"https://theprotocol.it/filtry/{cloudName};t")
+        elementProtocl = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "su6nd6p"))
+        )
+        quantityProtocol = re.search(r'(\d{1,3}(?:\s\d{3})*)', elementProtocl.text)
+        if quantityProtocol:
+            numberProtocol = quantityProtocol.group(1)
+            numberProtocol = numberProtocol.replace(" ", "")
+            cloudName = cloudName.replace("+", " ")
+            print(f"The number of offers for {cloudName} in theProtocol is: {numberProtocol}")
         else:
-            driver.get(f"https://nofluffjobs.com/pl/{cloudName}")
-            WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "list-container"))
-            )
-            link_elements = driver.find_elements(By.CLASS_NAME, "list-container a")
-            quantityNFJ = len(link_elements)
-            print(f"The number of offers for {cloudName} in No Fluff Jobs is: {quantityNFJ}")
+            print("No valid number found in the text.")
 
-            return quantityNFJ
-    
-                
+        return numberProtocol
+        
 choiceCloud()
 
