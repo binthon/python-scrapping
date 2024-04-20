6c6ecd402a79e5ad669a42deef41cedeebda1c21
binthon <jakub@medynwp.pl>
2024-04-20 15:35:45 +0200

    works

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import sqlite3
 import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
@@ -18,7 +17,7 @@ def run_tkinter_app():
 tkinter_thread = Thread(target=run_tkinter_app)
 tkinter_thread.start()
 
-def get_data(cloudName):
+def getDataCloud(cloudName):
     if cloudName is None:
         return pd.DataFrame()
     with open('jobs_offers.json', 'r') as file:
@@ -29,7 +28,7 @@ def get_data(cloudName):
     if not provider_data:
         return pd.DataFrame()
 
-
+    provider_data.pop('all', None)
     df = pd.DataFrame([provider_data])
     df = df.rename(columns={
         'pracuj': 'quantityPracuj', 
@@ -37,30 +36,32 @@ def get_data(cloudName):
         'nfj': 'quantityNFJ', 
         'theprotocol': 'numberProtocol'
     })
-    df['quantityAll'] = df.sum(axis=1)
+
     return df
 
-def create_plot(df, cloudName):
-    df['Total'] = df['quantityPracuj'] + df['numberJoin'] + df['quantityNFJ'] + df['numberProtocol']
+def createPlotCloud(df, cloudName):
+    df['quantityAll'] = df[['quantityPracuj', 'numberJoin', 'quantityNFJ', 'numberProtocol']].sum(axis=1)
+
     data = {
         'Pracuj.pl': df['quantityPracuj'].sum(),
         'JustJoin': df['numberJoin'].sum(),
         'No Fluff Jobs': df['quantityNFJ'].sum(),
         'theProtocol': df['numberProtocol'].sum()
     }
-    total = sum(data.values())
+
+    total = df['quantityAll'].iloc[0]
     
     sorted_data = sorted(data.items(), key=lambda item: item[1], reverse=True)
     labels = [f"{label} ({(size / total) * 100:.1f}%)" for label, size in sorted_data if size > 0]
     sizes = [size for label, size in sorted_data if size > 0]
-    legend_labels = [f"{label} - {size} offers" for label, size in sorted_data if size > 0]
+    legend_labels = [f"{label} - {size} offers" for label, size in sorted_data]
+
     plt.figure(figsize=(8, 6))
-    plt.title(f"Podział ofert dla {cloudName}")
+    plt.title(f"Job Offers Distribution for {cloudName}")
     wedges, texts = plt.pie(sizes, labels=labels, startangle=140)
     plt.legend(wedges, legend_labels, loc="center left", bbox_to_anchor=(1, 0.8))
-    sum_offers = int(df['quantityAll'])
     plt.axis('equal')
-    plt.text(0.5, -0.1, f"Total offers for {cloudName} is: {sum_offers}", horizontalalignment='center', fontsize=12, transform=plt.gca().transAxes)
+    plt.text(0.5, -0.1, f"Total offers for {cloudName}: {total}", horizontalalignment='center', fontsize=12, transform=plt.gca().transAxes)
     
     buf = BytesIO()
     plt.savefig(buf, format='png', bbox_inches='tight')
@@ -71,60 +72,40 @@ def create_plot(df, cloudName):
 
     return image_base64
 
-def get_dataWebsite(websiteName):
-    if websiteName is None:
-        return pd.DataFrame(), 0  
+def getDataWebsite(jobPlatform):
+    if jobPlatform is None:
+        return pd.DataFrame()
     with open('jobs_offers.json', 'r') as file:
         data = json.load(file)
     
-    website_data = data['offerswebsite'].get(websiteName, {})
+    platform_data = data['offerswebsite'].get(jobPlatform, {})
     
-    if not website_data:
-        return pd.DataFrame(), 0
+    if not platform_data:
+        return pd.DataFrame()
 
-    dw = pd.DataFrame(list(website_data.items()), columns=['portal_name', 'quantity'])
-    dwSum = dw['quantity'].sum()
+    total_offers = platform_data.pop('all', None)
+    df = pd.DataFrame(list(platform_data.items()), columns=['CloudProvider', 'Offers'])
+    return df, total_offers
 
-    return dw, dwSum
+def createPlotWebsite(df, total_offers, jobPlatform):
 
+    df = df.sort_values(by='Offers', ascending=False)
 
-def create_plot_website(dw, dwSum, websiteName):
-    if dw.empty:
-        return None
+    plot_data = df[df['Offers'] > 0]
 
-    filtered_dw = dw[dw['portal_name'] != 'all']
+    labels = [f"{row['CloudProvider']} ({row['Offers'] / total_offers * 100:.1f}%)" for index, row in plot_data.iterrows()]
+    sizes = plot_data['Offers'].tolist()
     
-    filtered_dw = filtered_dw.sort_values(by='quantity', ascending=False)
-
-    if filtered_dw.empty:
-        return None
-
-    chart_data = filtered_dw[filtered_dw['quantity'] > 0]
-    sizes = chart_data['quantity'].tolist()
-    labels_with_percents = [
-        f"{row['portal_name']} ({(row['quantity'] / dwSum) * 100:.1f}%)"
-        for _, row in chart_data.iterrows()
-    ]
-
-    legend_labels = [
-        f"{row['portal_name']} - {row['quantity']} offers"
-        for _, row in filtered_dw.iterrows()
-    ]
+   
+    legend_labels = [f"{row['CloudProvider']} - {row['Offers']} offers" for index, row in df.iterrows()]
 
     plt.figure(figsize=(8, 6))
-    plt.title(f"Podział ofert na {websiteName}")
-    wedges, texts = plt.pie(sizes, labels=labels_with_percents, startangle=140)
-    
-    custom_wedges = [plt.Patch(color=plt.cm.viridis(i/len(sizes)), label=label)
-                     for i, label in enumerate(legend_labels)]
-    
-    plt.legend(custom_wedges, legend_labels, loc="center left", bbox_to_anchor=(1, 0.8))
+    plt.title(f"Job Offers Distribution for {jobPlatform}")
+    wedges, texts = plt.pie(sizes, labels=labels, startangle=140)
+    plt.legend(wedges, legend_labels, loc="center left", bbox_to_anchor=(1, 0.8))
     plt.axis('equal')
-
+    plt.text(0.5, -0.1, f"Total offers for {jobPlatform}: {total_offers}", horizontalalignment='center', fontsize=12, transform=plt.gca().transAxes)
     
-    adjusted_total = filtered_dw['quantity'].sum()
-    plt.text(0.5, -0.1, f"Łączna liczba ofert na {websiteName} to: {adjusted_total}", horizontalalignment='center', fontsize=12, transform=plt.gca().transAxes)
-
     buf = BytesIO()
     plt.savefig(buf, format='png', bbox_inches='tight')
     plt.close()
@@ -133,8 +114,6 @@ def create_plot_website(dw, dwSum, websiteName):
     buf.close()
 
     return image_base64
-
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -143,12 +122,12 @@ def index():
     plot = None
     plotWebiste = None
     if cloudName:
-        df = get_data(cloudName)
-        plot = create_plot(df, cloudName)
+        df = getDataCloud(cloudName)
+        plot = createPlotCloud(df, cloudName)
         return render_template('index.html', plot=plot, cloudName=cloudName)
     elif websiteName:
-        dw, dwSum = get_dataWebsite(websiteName) 
-        plotWebiste = create_plot_website(dw, dwSum, websiteName)  
+        dw, dwSum = getDataWebsite(websiteName) 
+        plotWebiste = createPlotWebsite(dw, dwSum, websiteName)  
         return render_template('index.html', plotWebiste=plotWebiste, websiteName=websiteName)
     
     return render_template('index.html')
